a0782c103c6dd19f63f75badfd81d99d26ac4c22
jgantayat <gantayatjanmajaya@gmail.com>
2026-01-08 00:36:57 +0530

    kafka-4x-springboot-producer-consumer

---
+++ consumer-service/src/main/java/com/kafka/consumer_service/service/KafkaConsumerService.java
@@ -0,0 +1,14 @@
+package com.kafka.consumer_service.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "demo-topic", groupId = "demo-group")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+}
+
---
+++ producer-service/src/main/java/com/kafka/producer_service/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.kafka.producer_service.controller;
+
+import com.kafka.producer_service.service.KafkaProducerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    private final KafkaProducerService producerService;
+
+    public KafkaController(KafkaProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping("/publish")
+    public String publish(@RequestParam String message) {
+        producerService.sendMessage(message);
+        return "Message sent";
+    }
+}
